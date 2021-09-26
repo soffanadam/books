c0841bbe5abedf2a3827417f062be217a3094500
Soffan Adam <soffanadam@gmail.com>
2021-09-26 18:04:16 +0700

    eslint, prettier and stylelint

---
+++ @types/index.d.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare interface PlainObject<T = any> {
+  [key: string]: T
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import eslint from 'vite-plugin-eslint'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslint({
+      include: ['./src/*.ts', './src/*.vue'],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   server: {
     host: true,
   },
